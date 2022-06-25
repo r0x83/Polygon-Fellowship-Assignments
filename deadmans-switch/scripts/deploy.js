ed67c9aa025de967699d3fa3a685613a9c5a232c
imroshin <imroshin@gmail.com>
2022-06-25 21:01:58 +0530

    updated beneficiary

--- deadmans-switch/scripts/deploy.js
+++ deadmans-switch/scripts/deploy.js
@@ -2,7 +2,7 @@ async function main() {
     const deadmansSwitch = await ethers.getContractFactory("deadmansSwitch");
  
     // Start deployment, returning a promise that resolves to a contract object
-    const deadmans_Switch = await deadmansSwitch.deploy("Hello World!");
+    const deadmans_Switch = await deadmansSwitch.deploy("0xC81A20e3F8EBC43a317942a85a3968B79af20a30");
     console.log("Contract deployed to address:", deadmans_Switch.address);
  }
  
